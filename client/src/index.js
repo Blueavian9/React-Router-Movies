d62856a5e9fb016eedfbe0b33659a8f9f94270b7
cesar.aguilar <Blueavian9@gmail.com>
2022-10-12 02:39:31 -0700

    initial

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Movie from './pages/Movie.js';
+import MovieCard from './pages/Movie.js';
+import MovieList from './pages/Movie.js';
+import SavedList from './pages/SavedList.js';
+import Server from './pages/server.js';
 import './index.css';
 import App from './App';
 
+
+export default function App() {
+    return (
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            Route path="Movie.js" element={<Movie.js />} />
+            Route path="MovieCard.js" element={<MovieCard.js />} />
+            Route path="MovieList.js" element={<MovieList.js />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+    )
+}
 // You'll need to wrap <App /> for routing to work
 ReactDOM.render(<App />, document.getElementById('root'));
