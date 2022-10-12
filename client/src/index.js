431b847ba0f06c18ee75289cb34f0c48b297a9fc
cesar.aguilar <Blueavian9@gmail.com>
2022-10-12 03:34:07 -0700

    initial

--- client/src/index.js
+++ client/src/index.js
@@ -7,26 +7,30 @@ import MovieList from './pages/Movie.js';
 import SavedList from './pages/SavedList.js';
 import Server from './pages/server.js';
 import './index.css';
-import App from './App';
+import App from './pages/App';
 
 
-export default function App() {
-    return (
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="Movie.js" element={<Movie.js />} />
-            <Route path="MovieCard.js" element={<MovieCard.js />} />
-            <Route path="MovieList.js" element={<MovieList.js />} />
-            <Route path="*" element={<NoPage />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-    );
+export default App(); {
+  return (
+    <div className="wrapper">
+        <h1>App</h1>
+    </div>
+  );
+}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="Movie.js" element={<Movie.js />} />
+        <Route path="MovieCard.js" element={<MovieCard.js />} />
+        <Route path="MovieList.js" element={<MovieList.js />} />
+        <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 // You'll need to wrap <App /> for routing to work
 
-const root = ReactDOM.createRoot(
-root.render(<App />, document.getElementById('root')));
+const root = ReactDOM.createRoot(root.render(<App />, document.getElementById('root')));
