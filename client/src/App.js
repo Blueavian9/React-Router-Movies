6acaed2f317edbec28a961551a5a64bad474124e
cesar.aguilar <Blueavian9@gmail.com>
2022-10-16 12:08:56 -0700

    initial

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import MovieList from './Movies/MovieList';
 import SavedList from './Movies/SavedList';
-
-<header>
-  <Link to="/">Home</Link>
-  <Link to="/movie/1">Movie1</Link>
-  <Link to="/movie/2">Movie2</Link>
-  <Link to="/movie/3">Movie3</Link>
-  <Link to="/movie/4">Movie4</Link>
-  <Link to="/movie/5">Movie5</Link>
-</header>
-<main>
-
-  <Switch>
-    <Route exact path="/">
-        <h2>Welcome please select your Movie</h2>
-    </Route>
-    <Route exact path="movie1">
-      <movie1 />
-    </Route>
-    <Route exact path="movie2">
-      <movie2 />
-    </Route>
-    <Route exact path="movie3">
-      <movie3 />  
-    </Route>    
-    <Route exact path="movie4">
-      <movie4 />
-    </Route>
-    <Route exact path="movie5">
-      <movie5 />
-    </Route>
-  </Switch>
-</main>
-
-<footer>
-  <p>&copy; Copoyright 2022 Movies</p>
-</footer>
-
+import Movie from './Movies/Movie';
 
 export default function App () {
-  const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movieList, setMovieList] = useState([]);
 
   useEffect(() => {
@@ -49,6 +13,7 @@ export default function App () {
       axios
         .get('http://localhost:5001/api/movies') // Study this endpoint with Postman
         .then(response => {
+          setMovieList(response.data);
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
         })
@@ -59,15 +24,15 @@ export default function App () {
     getMovies();
   }, []);
 
-  const addToSavedList = id => {
-    // This is stretch. Prevent the same movie from being "saved" more than once
-  };
-
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
-
-      <div>Replace this Div with your Routes</div>
+      <Route exact path="/">
+        <MovieList movie={movieList} />  
+      </Route>
+      <Route path="/movies/:id">
+        <Movie />
+      </Route>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,33 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import Movie from './pages/Movie.js';
-import MovieCard from './pages/MovieCard.js';
-import MovieList from './pages/MovieList.js';
-import SavedList from './pages/SavedList.js';
-import Server from './pages/server.js';
-import'./index.css';
-import App from './components/App/App';
 
-export default App() { 
-  return (
-    <div className="wrapper">
-       <h1>App</h1>
-    
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="Movie.js" element={<Movie.js />} />
-        <Route path="MovieCard.js" element={<MovieCard.js />} />
-        <Route path="MovieList.js" element={<MovieList.js />} />
-        <Route path="*" element={<NoPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
- </div>
-  );
+import'./index.css';
+import App from './App';
+
+import { BrowserRouter as Router } from 'react-router-dom';
 
 // You'll need to wrap <App /> for routing to work
-const root = ReactDOM.createRoot(root.render(<App />, document.getElementById('root')));
-}
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>
+  , document.getElementById('root'));
+
+  /**
+   * history, match, location
+   * 
+   * 
+   */
