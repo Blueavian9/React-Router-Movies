b6bc77f3da14508c968fd25766d8cc25f2afbbc2
Cesar Aguilar <Blueavian9@gmail.com>
2023-09-09 18:37:09 -0700

    Initial Update

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,35 +1,9 @@
-import React from "react";
-
-const MovieCard = (props) => {
-  // Ensure that props.movie.stars is an array, and provide an empty array as a fallback if it's not defined.
-  const stars = Array.isArray(props.movie.stars) ? props.movie.stars : [];
-
-  return (
-    <div className="movie-card">
-      <h2>{props.movie.title}</h2>
-      <div className="movie-director">
-        Director: <em>{props.movie.director}</em>
-      </div>
-      <div className="movie-movie-card">
-        MovieCard: <strong>{props.movie.movie - MovieCard.js}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map((star) => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
-  );
-};
-
-export default MovieCard;
-
-// My Original Code
 // import React from "react";
 
 // const MovieCard = (props) => {
+//   // Ensure that props.movie.stars is an array, and provide an empty array as a fallback if it's not defined.
+//   const stars = Array.isArray(props.movie.stars) ? props.movie.stars : [];
+
 //   return (
 //     <div className="movie-card">
 //       <h2>{props.movie.title}</h2>
@@ -37,11 +11,11 @@ export default MovieCard;
 //         Director: <em>{props.movie.director}</em>
 //       </div>
 //       <div className="movie-movie-card">
-//         MovieCard.js: <strong>{props.movie.movie-card}</strong>
+//         MovieCard.js: <strong>{props.movie.MovieCard.js}</strong>
 //       </div>
 //       <h3>Actors</h3>
 
-//       {props.movie.stars.map((star) => (
+//       {stars.map((star) => (
 //         <div key={star} className="movie-star">
 //           {star}
 //         </div>
@@ -51,3 +25,29 @@ export default MovieCard;
 // };
 
 // export default MovieCard;
+
+/* My Original Code */
+import React from "react";
+
+const MovieCard = (props) => {
+  return (
+    <div className="movie-card">
+      <h2>{props.movie.title}</h2>
+      <div className="movie-director">
+        Director: <em>{props.movie.director}</em>
+      </div>
+      <div className="movie-movie-card">
+        MovieCard.js: <strong>{props.movie.MovieCard.js}</strong>
+      </div>
+      <h3>Actors</h3>
+
+      {props.movie.stars.map((star) => (
+        <div key={star} className="movie-star">
+          {star}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default MovieCard;
