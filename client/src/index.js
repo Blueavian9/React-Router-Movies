61d545a82180c9cdd944f96c9ebefccbd093a81f
Cesar Aguilar <Blueavian9@gmail.com>
2023-11-14 13:46:44 -0800

    Initial commit

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
 
-import './index.css'
-import App from './App'
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
-	<Router>
-		<App />
-	</Router>,
-	document.getElementById('root')
-)
+  <BrowserRouter>
+    {" "}
+    {/* Wrap your App component with BrowserRouter */}
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+
+// Old code
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import { BrowserRouter as Router } from 'react-router-dom'
+
+// import './index.css'
+// import App from './App'
+
+// ReactDOM.render(
+// 	<Router>
+// 		<App />
+// 	</Router>,
+// 	document.getElementById('root')
+// )
